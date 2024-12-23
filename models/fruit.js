fe3eb4088cd3af615471c99d3c2a872c38343c54
HussainWorld <hussain.university.poly@gmail.com>
2024-12-23 20:42:59 +0300

    connect to mangoose and add first model

---
+++ models/fruit.js
@@ -0,0 +1,14 @@
+// models/fruit.js
+const mongoose = require('mongoose');
+
+//create the schema
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  isReadyToEat: Boolean
+});
+
+//register the model
+const Fruit = mongoose.model('Fruit', fruitSchema)
+
+//share it with the rest of the app
+module.exports = Fruit;
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 // Here is where we import modules
 // We begin by loading Express
+require('dotenv').config();
+require('./config/database.js');
+
 const express = require('express');
 const morgan = require('morgan');
+
+//Models
+// Import the Fruit model
+const Fruit = require("./models/fruit.js");
+
 
 const app = express();
 
@@ -9,6 +17,11 @@ const app = express();
 app.use(morgan('dev'));
 
 // ROUTES
+// GET /
+app.get("/", async (req, res) => {
+  res.render('index.ejs')
+});
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
