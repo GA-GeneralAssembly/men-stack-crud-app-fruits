61ca64e0083021eef16c66feafeba8b3143fb690
HussainWorld <hussain.university.poly@gmail.com>
2024-12-24 18:53:33 +0300

    add the index and the create flow

--- server.js
+++ server.js
@@ -16,19 +16,38 @@ const app = express();
 // MIDDLEWARE
 app.use(morgan('dev'));
 
+app.use(express.urlencoded({ extended: false }));
+
+
 // ROUTES
 // GET /
 app.get("/", async (req, res) => {
   res.render('index.ejs')
 });
 
-// server.js
 
 // GET /fruits/new
 app.get("/fruits/new", (req, res) => {
   res.render('fruits/new.ejs')
 });
 
+
+app.get("/fruits", async (req, res) => {
+  const allFruits = await Fruit.find();
+  res.render("fruits/index.ejs", { fruits: allFruits });
+});
+
+
+// POST /fruits
+app.post("/fruits", async (req, res) => {
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  await Fruit.create(req.body);
+  res.redirect("/fruits"); //redirct is always a get request 
+});
 
 
 app.listen(3000, () => {
